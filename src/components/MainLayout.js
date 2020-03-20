d8e0967b0a1e86cef36e51b23ff0f507e52442cc
ARMY <dfdfg>
2020-03-21 01:18:46 +0700

    Update

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, PageHeader,Button } from "antd";
 import FormRegister from "../components/FormRegister";
 import SubMenu from "../components/SubMenu";
-import DashBoard from "../components/DashBoard"
-import FormMemorize from '../components/FormMemorize'
+import DashBoard from "../components/DashBoard";
+import FormMemorize from "../components/FormMemorize";
 const { Header, Content, Footer, Sider } = Layout;
 
-const MainLayout = (props) => {
+const MainLayout = props => {
   const { history } = props;
   return (
     <Layout>
@@ -28,16 +28,25 @@ const MainLayout = (props) => {
         <Header
           className="site-layout-sub-header-background"
           style={{ padding: 0, background: "white" }}
-        />
+        >
+          <PageHeader
+            className="site-page-header"
+            // onBack={() => null}
+            title="Your are"
+            subTitle="Admin"
+            extra={[
+              <Button key="1" type="danger">
+                Logout
+              </Button>,
+            ]}
+          />
+        </Header>
         <Content style={{ margin: "24px 16px 0" }}>
-        {/* <FormRegister></FormRegister> */}
-        {/* <DashBoard></DashBoard> */}
           {props.children}
           <div
             className="site-layout-background"
             style={{ padding: 24, minHeight: 360 }}
-          >
-          </div>
+          ></div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
           Ant Design ©2018 Created by Ant UED
