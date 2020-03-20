1dce73333fd7d0705f03f318c74ca003f6f5444a
ARMY <dfdfg>
2020-03-20 21:36:39 +0700

    add Link and Redirec

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 
 import FormRegister from "../components/FormRegister";
-import DashBoard from "../components/DashBoard"
-import FormMemorize from '../components/FormMemorize'
+import DashBoard from "../components/DashBoard";
+import FormMemorize from "../components/FormMemorize";
 
-import generateComponentWithMainLayout from '../hoc/generateComponentWithMainLayout';
+import generateComponentWithMainLayout from "../hoc/generateComponentWithMainLayout";
 
 const FormRegisterWithLayout = generateComponentWithMainLayout(FormRegister);
 const DashBoardWithLayout = generateComponentWithMainLayout(DashBoard);
 const FormMemorizeWithLayout = generateComponentWithMainLayout(FormMemorize);
 
 export default () => (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/dashboard">
-          <DashBoardWithLayout />
-        </Route>
-        <Route exact path="/add">
-          <FormRegisterWithLayout />
-        </Route>
-        <Route exact path="/memorize">
-          <FormMemorizeWithLayout />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/dashboard">
+        <DashBoardWithLayout />
+      </Route>
+      <Route exact path="/add">
+        <FormRegisterWithLayout />
+      </Route>
+      <Route exact path="/memorize">
+        <FormMemorizeWithLayout />
+      </Route>
+      <Redirect from="/" to="/dashboard" />
+    </Switch>
+  </BrowserRouter>
 );
