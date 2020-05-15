4febf95afc057e439050173cd77eea488997445a
NantipatSoften <keymasterviriya1150@gmail.com>
2020-05-15 16:16:18 +0700

    Data Table

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,25 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Title from "./shared/Title";
 import SelectBox from "./shared/SelectBox";
 import FormIcon from "./shared/FormIcon";
 import Colume from "./shared/Colume";
 import RowsUser from "./shared/RowsUser";
 // import { data } from "./mock/table";
+import Table from "./shared/Table";
 import fetch from "node-fetch";
+import axios from "axios";
+
 const Board = (props) => {
-  const [data, setData] = useState({ hits: [] });
+  const [data, setData] = useState({ users: [] });
+  const columns = ["User", "facebook", "Tel", "dateBelieve", "Address", "Action"];
   useEffect(async () => {
-    const result = await fetch("http://159.89.204.113:3000/users");
-    console.log("result=",result);
-    
-    setData(result.data);
+    const result = await axios("http://159.89.204.113:3000/users");
+
+    setData({
+      users: result.data,
+    });
   }, []);
+
   return (
     <React.Fragment>
       <Title name="Table" />
@@ -24,30 +30,7 @@ const Board = (props) => {
       </div>
       <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-          <table className="min-w-full leading-normal">
-            <thead>
-              <tr>
-                <Colume name="User" />
-                <Colume name="Role" />
-                <Colume name="Created at" />
-                <Colume name="Status" />
-                <Colume name="Action" />
-              </tr>
-            </thead>
-            <tbody>
-              {data.hits}
-              {/* {data.map((user, i) => (
-                <RowsUser
-                  key={i}
-                  index={i}
-                  user={user}
-                  editUser={props.dispatchEditUser}
-                  delUser={props.dispatchDelUser}
-                  history={props.history}
-                />
-              ))} */}
-            </tbody>
-          </table>
+          <Table columns={columns} data={data} props={props} />
           <div className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
             <span className="text-xs xs:text-sm text-gray-900">Showing 1 to 4 of 50 Entries</span>
             <div className="inline-flex mt-2 xs:mt-0">
--- src/components/shared/RowsUser.js
+++ src/components/shared/RowsUser.js
@@ -15,25 +15,30 @@ const RowsUser = ({ index, user, editUser, delUser, history }) => {
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
           <div className="flex items-center">
             <div className="flex-shrink-0 w-10 h-10">
-              <img className="w-full h-full rounded-full" src={user.img} alt="" />
+              <img className="w-full h-full rounded-full" src={user.pictureProfile} alt="" />
             </div>
             <div className="ml-3">
-              <p className="text-gray-900 whitespace-no-wrap">{user.name}</p>
+              <p className="text-gray-900 whitespace-no-wrap">
+                {user.firstName} `{user.nickname}` {user.lastName}
+              </p>
             </div>
           </div>
         </td>
 
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p className="text-gray-900 whitespace-no-wrap">{user.role}</p>
+          <p className="text-gray-900 whitespace-no-wrap">{user.facebook}</p>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p className="text-gray-900 whitespace-no-wrap">{user.date}</p>
+          <p className="text-gray-900 whitespace-no-wrap">{user.Tel}</p>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
           <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
-            <span aria-hidden className={"absolute inset-0 opacity-50 rounded-full " + user.color}></span>
-            <span className="relative">{user.status}</span>
+            <span aria-hidden className={"absolute inset-0 opacity-50 rounded-full"}></span>
+            <span className="relative">{user.dateBelieve}</span>
           </span>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">{user.Address}</p>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
           {/* <button className="bg-blue-500 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
