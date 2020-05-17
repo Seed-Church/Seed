302c22d24aa90fa1ab19425db1cd34417000daef
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-05-17 19:37:46 +0700

    Add middlewares

--- src/store/ConfigStore.js
+++ src/store/ConfigStore.js
@@ -1,11 +1,15 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import Users from "../reducer/Users";
-//const middlewares = applyMiddleware(mylogger, thunk)
+const middlewares = applyMiddleware(thunk);
+
 export default () => {
   const store = createStore(
     combineReducers({
       Users,
-    })
+    }),
+    {},
+    compose(middlewares)
   );
 
   return store;
