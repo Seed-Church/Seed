7e71d378efbd8807a83801bcedd3722c1d5048b3
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-06-20 08:52:11 +0700

    rename

--- src/components/shared/SelectBox.js
+++ src/components/shared/SelectBox.js
@@ -15,7 +15,7 @@ const SelectBox = ({ label, values, name, isEditing, previousValue, register }) 
               {previousValue.name}
             </option>
           ) : null}
-          <ListOption list={values} previousValue={previousValue} />
+          <LoadOption list={values} previousValue={previousValue} />
         </select>
         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
           <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -27,7 +27,7 @@ const SelectBox = ({ label, values, name, isEditing, previousValue, register }) 
   );
 };
 
-const ListOption = ({ list, previousValue }) => {
+const LoadOption = ({ list, previousValue }) => {
   if (!list) {
     return null;
   }
