aab0e54346b4e3c8c96b316aa6f28a4d4bbc8b30
NantipatSoften <keymasterviriya1150@gmail.com>
2020-07-08 16:34:02 +0700

    new icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
       <div className="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
         <ul className="list-reset flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
           <Menu href="/form" name="Form" icon="fas fa-tasks pr-0 md:pr-3" />
-          <Menu href="/card" name="Card" icon="fa fa-envelope pr-0 md:pr-3" />
+          <Menu href="/card" name="ภาพรวม" icon="fab fa-angellist pr-0 md:pr-3" />
           <Menu href="/board" name="ตาราง" icon="fas fa-chart-area pr-0 md:pr-3" />
-          <Menu href="/blog" name="บล็อก" icon="fa fa-wallet pr-0 md:pr-3" />
-          <Menu href="/usercard" name="UserCard" icon="fa fa-wallet pr-0 md:pr-3" />
+          <Menu href="/blog" name="บล็อก" icon="fas fa-bible pr-0 md:pr-3" />
+          <Menu href="/usercard" name="การ์ด" icon="far fa-address-card pr-0 md:pr-3" />
         </ul>
       </div>
     </div>
