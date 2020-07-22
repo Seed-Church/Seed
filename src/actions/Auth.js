dd59dd3f63075d10be8611739f6e009e1f02021e
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-07-22 16:01:40 +0700

    solve problem reload login

--- src/actions/Auth.js
+++ src/actions/Auth.js
@@ -39,7 +39,10 @@ const headers = {
   "Content-Type": "application/json",
 };
 export const getToken = (data) => generateAction(AUTH, "POST", authGetTokenHuman, actionGruop, `/getToken`, JSON.stringify(data), headers);
-const token = {
-  Authorization: `Bearer ${localStorage.getItem("token")}`,
-};
-export const getData = () => generateAction(AUTH, "GET", authGetDataHuman, actionGruop, `/getData`, null, token);
+// const token = {
+//   Authorization: `Bearer ${localStorage.getItem("token")}`,
+// };
+export const getData = () =>
+  generateAction(AUTH, "GET", authGetDataHuman, actionGruop, `/getData`, null, {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  });
